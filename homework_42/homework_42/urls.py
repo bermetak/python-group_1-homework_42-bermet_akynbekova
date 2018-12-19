fd72ff8c6dc651eb124a0a5c3bd097d15914e890
bermet <akynbekovabermet@gmail.com>
2018-12-19 19:29:12 +0600

    Added article_view, article_detail, user_view, user_detail

--- homework_42/homework_42/urls.py
+++ homework_42/homework_42/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import ArticleListView, ArticleDetailView, UserListView, UserDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ArticleListView.as_view(), name='article_list'),
+    path('articles/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
+    path('users', UserListView.as_view(), name='user_list'),
+    path('users/<int:pk>', UserDetailView.as_view(), name='user_detail'),
+
 ]
--- homework_42/webapp/views.py
+++ homework_42/webapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView, DetailView, CreateView, TemplateView, View, RedirectView
+from webapp.models import User, Article, Comment, Rating
+from django.urls import reverse_lazy
 # Create your views here.
+
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+class UserListView(ListView):
+    model = User
+    template_name = 'user_list.html'
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'user_detail.html'
